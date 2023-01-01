2fccd5c7afa73f8a974a20e9f67c6982841dae1e
Jym Bocala <jym.bocala@gmail.com>
2023-01-01 23:13:42 +1100

    add basic structure for the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  return (
+      <div className="app">
+          <h1>Speed Typing Game</h1>
+          <textarea />
+          <h4>Time remaining: 0</h4>
+          <button>Start Game</button>
+          <h1>Word count: 0</h1>
+      </div>
+  )
+}
