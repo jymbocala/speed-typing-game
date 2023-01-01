af94868e25303eb717d4637d913e676e0945f1b9
Jym Bocala <jym.bocala@gmail.com>
2023-01-02 00:22:50 +1100

    add text state and function to setText

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react"
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
   return (
-      <div className="app">
-          <h1>Speed Typing Game</h1>
-          <textarea />
-          <h4>Time remaining: 0</h4>
-          <button>Start Game</button>
-          <h1>Word count: 0</h1>
-      </div>
-  )
+    <div className="app">
+      <h1>Speed Typing Game</h1>
+      <textarea 
+        onChange={handleChange}
+        value={text}
+      />
+      <h4>Time remaining: 0</h4>
+      <button>Start Game</button>
+      <h1>Word count: 0</h1>
+    </div>
+  );
 }
