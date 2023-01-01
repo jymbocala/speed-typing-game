f391d767a6b100fedd12fee62b426bccc2fa53e4
Jym Bocala <jym.bocala@gmail.com>
2023-01-02 02:18:19 +1100

    add time remaining state and useEffect method each time timeRemaining updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -10,19 +11,26 @@ export default function App() {
   }
 
   function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
   }
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
 
   return (
     <div className="app">
       <h1>Speed Typing Game</h1>
-      <textarea 
-        onChange={handleChange}
-        value={text}
-      />
-      <h4>Time remaining: 0</h4>
-      <button onClick={() => console.log(calculateWordCount(text))}>Start Game</button>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => console.log(calculateWordCount(text))}>
+        Start Game
+      </button>
       <h1>Word count: 0</h1>
     </div>
   );
